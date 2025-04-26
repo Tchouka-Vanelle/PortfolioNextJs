5dbff3a50c40ec7d896ecaff21e1c82decca466f
Tchouka-Vanelle <76223426+Tchouka-Vanelle@users.noreply.github.com>
2025-04-27 01:22:21 +0200

    change role in work

--- app/_components/Work.tsx
+++ app/_components/Work.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
 
 export const WORKS: WorkProps[] = [
-  {
-    image: '../images/nkah.jpg',
-    title: 'MULTIPURPOSE INTERNSHIP - COMMERCIAL ASSISTANCE',
-    role: 'intern',
-    date: '06/2021-09/2021',
-    url: 'https://www.groupnkahengineering.com/',
-  },
+ 
   {
     image: '../images/carrier.png',
     title: 'FULLSATCK DEVELOPER IN APPRENTICESHIP',
-    role: 'infull-stack developera lternatetern',
+    role: "Développement front-end en React.js, conception d'API GraphQL en Rust, application des méthodes Scrum, gestion des tâches avec GitFlow et Jira, mise en place de tests, et collaboration avec les Product Owners pour améliorer la qualité du code et des fonctionnalités.",
     date: '2023-2025',
     url: 'https://www.carrier.com/carrier/en/worldwide/',
+  },
+  {
+    image: '../images/nkah.jpg',
+    title: 'MULTIPURPOSE INTERNSHIP - COMMERCIAL ASSISTANCE',
+    role: "Participation à la prospection, soutien aux tâches quotidiennes de l'entreprise et gestion du matériel commercialisé.",
+    date: '06/2021-09/2021',
+    url: 'https://www.groupnkahengineering.com/',
   },
 ];
 
